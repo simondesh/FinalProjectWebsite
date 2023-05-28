58f6fb427ec859765479b4f444bede54c27b537f
ShuyuCaoCathy <127530743+ShuyuCaoCathy@users.noreply.github.com>
2023-05-28 15:53:26 +0100

    Update 3_📊_DataFrame_Demo.py

--- pages/3_📊_DataFrame_Demo.py
+++ pages/3_📊_DataFrame_Demo.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+df = pd.read_csv('df.csv')
 
 st.set_page_config(page_title="Our Dataframe", page_icon="📊")
 
 st.markdown("# Our DataFrame")
 st.sidebar.header("Our DataFrame")
 st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
+    """This page displays the dataframe we use to conduct the EDA and also the topic modeling. The specific data collection, cleaning, preprocessing, and wrangling is explained in the other part"""
 )
