8ad495e06b894214372801f11d4b7de08a5baa44
ShuyuCaoCathy <127530743+ShuyuCaoCathy@users.noreply.github.com>
2023-05-28 15:50:30 +0100

    Update 3_📊_DataFrame_Demo.py

--- pages/3_📊_DataFrame_Demo.py
+++ pages/3_📊_DataFrame_Demo.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
+st.set_page_config(page_title="Our Dataframe", page_icon="📊")
 
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
+st.markdown("# Our DataFrame")
+st.sidebar.header("Our DataFrame")
 st.write(
     """This demo shows how to use `st.write` to visualize Pandas DataFrames.
 (Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
