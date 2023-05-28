d256c1c9343c559a2d1dab63b0343fe71fcbed47
simon <simon.deshayes@hotmail.com>
2023-05-28 15:00:40 +0100

    setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Website for group project 
+
+ install module streamlit.
+ 
+ run the project : python -m streamlit run Home.py
+
+ ## ressources 
+
+ main concepts : https://docs.streamlit.io/library/get-started/main-concepts
+
+ multipages : https://docs.streamlit.io/library/get-started/multipage-apps (many examples so look at them pls)
---
+++ pages/3_📊_DataFrame_Demo.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+
+st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
+
+st.markdown("# DataFrame Demo")
+st.sidebar.header("DataFrame Demo")
+st.write(
+    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
+(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
+)
