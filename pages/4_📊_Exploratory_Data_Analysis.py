67512cc6cfc25678be69a123226d0b804b88ce07
Sijia-He <scarlett.he0116@qq.com>
2023-05-29 16:33:05 +0800

    new EDA.ipynb updated

---
+++ pages/4_📊_Exploratory_Data_Analysis.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+st.set_page_config(page_title="📈 EDA", page_icon="📈")
+
+st.markdown("# 📈 EDA")
+st.sidebar.header("📈 EDA")
+
+st.write(
+    """
+    In this part, we have conducted the analysis into two parts:
+    1. Analysis of 
+    2. Analysis of 
+    
+    In total, we have analysed how the variables below influence the popularity of the steam games:
+    
+    As also stated in the introduction, we have analysed the relationship between the variables and the rating of the games to find out the most important factors that influence the popularity of the games.
+    
+    What we got from the analysis is that the important features are:
+    1. 
+    2. 
+    3. 
+   """
+)
+
+
+st.subheader("EDA Part1 - Analysis of ")
+
+st.write(
+    """
+    In this part, we have conducted an analysis of 
+"""
+)
+
+st.subheader("EDA Part2 - Analysis of ")
+
+st.write(
+    """
+    In this part, we have conducted an analysis of 
+"""
+)
