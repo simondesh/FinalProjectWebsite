2c75861954837ab2cdb35be6629f8d0e3be2dcee
ShuyuCaoCathy <shuyu_cao@outlook.com>
2023-05-30 17:42:25 -0400

    deletion

--- pages/2_🌍_Introduction.py
+++ pages/2_🌍_Introduction.py
@@ -12,17 +12,14 @@ st.image(url, width=750)
 st.subheader("1) Project Introduction")
 st.write(
     """
-    Steam, which provides access to thousands of games, is one of the biggest platforms for game players, publishers and developers. New games released by the Steam always become the hot titles online, and millions of players are playing games via Steam every day. Until now, there are around games on Steam. The number of games just came out as of Septermber in 2022 reaches to 50,000. Apart from new games, many old games such as CSGO have made a legend in the computer game history. 
+    Steam is one of the biggest platforms for game players, publishers and developers. Game creation indeed is a great industry. Our team is curious about what makes games popular.
     
-    Among all the games, some are popular while others are not purchased by many. Game creation indeed is a great industry. Successfully creating a popular game can always make a fortune to the developers, which motivates many to join the market. Thus, our team is curious about what makes games popular. We would like to provide suggestions to creators, investors and publishers about how to produce a popular game from the dimension of genre, price, release year, and etc.
+    To begin with, we identify the popularity of game by rating, which was produced with negative and positive reviews by ourselves.
     
-    To begin with, we identify the popularity of game by rating, which was produced with negative and positive reviews by ourselves. The laster methodology part will further illustrate how we made it. 
+    We first conducted a correlation analysis to observe what variables might have an influence on the popularity of the game, and also discover the strong and positive relation between ratings and median number of owners to prove that these two variables can all represent the popularity.
 
-    For the project, we first conducted a correlation analysis to observe what variables might have an influence on the popularity of the game, and also discover the strong and positive relation between ratings and median number of owners to prove that these two variables can all represent the popularity.
+    After the correlation analysis, we selected release year; genres, categories and steam_spy_tags; price, and studied their influence on the popularity of the game. We also conducted a topic modelling on the effects of achievements, price, ratings, and required age.
 
-    After the correlation analysis, we selected the categorical and numeric features that we would like to explore further  -  release year; genres, categories and steam_spy_tags; price. 
-
-    The full analysis is divided into two parts. First part goes to the exploratory data analysis on the effects of release year, game type and pricing. In the second part, we use the XGBoost to build up a model to analyse the influence of the variables including achievements, price, ratings, and required age. The aim is to create a model to predict the popularity of games by forcasting their number of owners, with these varibles. You can see the small prediction application in the end of the topic modelling. Try to predict your games'popularity!
     """
 )
 
@@ -33,14 +30,13 @@ st.subheader("2) Website Structure")
 st.write(
     """
     As shown in the home page, we have many side views on the left for you to browse. 
-    
-    Specifically, we would like to introduce the project in the following order:
-    
+        
     **0. Data Frame Display**
     
-    In this part, you will see what data frame we produce from the data collection - `steam_clean.csv`. We will then show you the 'data_for_EDA.csv` which has gone through data preprocessing by removing non-english and non-windows games. This df is used for the EDA.
+    In this part, you will see what data frame we produce from the data collection - `steam_clean.csv`. We will then show you the `data_for_EDA.csv` which has gone through data preprocessing by removing non-english and non-windows games. This df is used for the EDA.
     
     We will also show you the `data_model.csv` which is used for the topic modelling.
+    
     
     **1. Introduction** 
     
--- pages/6_💪🏼_Limitations.py
+++ pages/6_💪🏼_Limitations.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="💪🏼 Limitations and Future Work", page_icon="💪🏼")
 
-st.markdown("# 💪🏼 Limitations and Future Work")
-
+st.markdown("# 💪🏼 Limitations")
 
 url = 'https://raw.githubusercontent.com/simondesh/FinalProjectWebsite/main/src/images/limitation.png'
 
@@ -12,17 +11,12 @@ st.image(url, width=500)
 st.write(
     """
     Now it is the time for some confesions for our limitations.
-    
-    Though with wholsome analysis, and we have gotten many pleasant results, this research undoubtedly has some limitations.
-    
-    Firstly, the data we grasped from the steam spy api has a sort of data loss issue. As the steam official website does not allow web scrapping and also limits non-authorised users to feach data by its API, we have to use steam_spy for more data, such as release year, positive and negative comments, and etc. The steam_spy api is created by nonofficial steam engineers, so that out of some unknown reasons, it has a data loss issue. It means that its data of the steam games is not complete. In the future work, we’d better try other means to call the api.
+        
+    Firstly, the data we grasped from the steam spy api has a sort of data loss issue. As the steam official website does not allow web scrapping and also limits non-authorised users to feach data by its API, we have to use steam_spy for more data, such as release year, positive and negative comments, and etc. The steam_spy api is created by nonofficial steam engineers, so that out of some unknown reasons, it has a data loss issue. 
 
     Secondly, the price of the games are not as same as when they were released. Steam often provides discount on the game, the price we get in the data frame is the current price as of the api calling date. There is no way to get track of their previous price one by one so that we cannot analyse the influence of discount or not and also are unable to use their original to produce the model. 
 
     Thirdly, out of privacy protection regulation, we cannot get some interesting data that could better for constructing the model. There is another website called SteamDB, which provides a more thorough data of the games, such as the price of the skins, xxx in the game. However, only academic scholars can apply for the authoried use for scraping the web. Our application is denied so we can only stick to the steam spy api. Also, steam official websites does not allow api call for private information of game players, developers and publishers. The meterial we can use is quite limited. 
     
-    Hence, in the future work, the first we need to solve is to get a more complete data. We can try to use other ways to call the steam spy api or try to get the authorisation of steamDB. Also, we can try to use other data sources, such as the data from the game developers and publishers. Also, we will try other machine learning models to see if we can get a better result.
-    
    """
 )
-
